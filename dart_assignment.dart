c6918a6a6d9472a9e7e9c4c3e271aab9b8d4394f
JohnWanjau <160610522+JohnWanjau@users.noreply.github.com>
2024-03-01 14:01:47 +0300

    Add files via upload

---
+++ dart_assignment.dart
@@ -0,0 +1,28 @@
+// Dart program demonstrating various data types
+
+void main() {
+  // Example of int data type
+  int age = 23;
+  print('Age: $age'); // Printing age
+
+  // Example of double data type
+  double height = 5.9;
+  print('Height: $height'); // Printing height
+
+  // Example of String data type
+  String name = 'John Wanjau';
+  print('Name: $name'); // Printing name
+
+  // Example of List data type
+  List<String> citieskenya = ['Nairobi', 'Mombasa', 'Nakuru'];
+  print('cities_kenya: $citieskenya'); // Printing list of fruits
+
+  // Example of Map data type
+  Map<String, dynamic> person = {
+    'name': 'Wanjau',
+    'age': 23,
+    'city': 'Nairobi',
+  };
+  print('Person: $person'); // Printing person details
+}
+  
